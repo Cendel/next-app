9f563d66405357beab6d4f270e8e2c64adcb0c66
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-03-23 23:35:48 +0100

    Client and Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>New Project</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
